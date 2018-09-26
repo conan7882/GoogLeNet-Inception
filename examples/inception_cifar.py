6a1c9014c6221c270cce31d06f93136c42bdf0f2
Qian Ge <qge2@ncsu.edu>
2018-09-26 13:54:27 -0400

    add inference for cifar dataset

--- examples/inception_cifar.py
+++ examples/inception_cifar.py
@@ -20,6 +20,7 @@ from src.helper.evaluator import Evaluator
 DATA_PATH = '/home/qge2/workspace/data/dataset/cifar/'
 SAVE_PATH = '/home/qge2/workspace/data/out/googlenet/cifar/'
 PRETRINED_PATH = '/home/qge2/workspace/data/pretrain/inception/googlenet.npy'
+IM_PATH = '../data/cifar/'
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -28,6 +29,8 @@ def get_args():
                         help='Train the model')
     parser.add_argument('--eval', action='store_true',
                         help='Evaluate the model')
+    parser.add_argument('--predict', action='store_true',
+                        help='Get prediction result')
     parser.add_argument('--finetune', action='store_true',
                         help='Fine tuning the model')
     parser.add_argument('--load', type=int, default=99,
@@ -41,6 +44,9 @@ def get_args():
                         help='Keep probability for dropout')
     parser.add_argument('--maxepoch', type=int, default=100,
                         help='Max number of epochs for training')
+
+    parser.add_argument('--im_name', type=str, default='.png',
+                        help='Part of image name')
 
     return parser.parse_args()
 
@@ -107,6 +113,40 @@ def evaluate():
         print('testing set:', end='')
         evaluator.accuracy(sess, valid_data)
 
+def predict():
+    FLAGS = get_args()
+    # Read Cifar label into a dictionary
+    label_dict = loader.load_label_dict(dataset='cifar')
+    # Create a Dataflow object for test images
+    image_data = loader.read_image(
+        im_name=FLAGS.im_name, n_channel=3,
+        data_dir=IM_PATH, batch_size=1, rescale=False)
+
+    # Create a testing GoogLeNet model
+    test_model = GoogLeNet_cifar(
+        n_channel=3, n_class=10, bn=True, sub_imagenet_mean=False)
+    test_model.create_test_model()
+
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, '{}inception-cifar-epoch-{}'.format(SAVE_PATH, FLAGS.load))
+        while image_data.epochs_completed < 1:
+            # read batch files
+            batch_data = image_data.next_batch_dict()
+            # get batch file names
+            batch_file_name = image_data.get_batch_file_name()[0]
+            # get prediction results
+            pred = sess.run(test_model.layers['top_5'],
+                            feed_dict={test_model.image: batch_data['image']})
+            # display results
+            for re_prob, re_label, file_name in zip(pred[0], pred[1], batch_file_name):
+                print('===============================')
+                print('[image]: {}'.format(file_name))
+                for i in range(5):
+                    print('{}: probability: {:.02f}, label: {}'
+                          .format(i+1, re_prob[i], label_dict[re_label[i]]))
+
 if __name__ == "__main__":
     FLAGS = get_args()
 
@@ -114,3 +154,5 @@ if __name__ == "__main__":
         train()
     if FLAGS.eval:
         evaluate()
+    if FLAGS.predict:
+        predict()
